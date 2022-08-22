96ef2b320c5fc481c61931d1d298b8430d45cff0
seaboyz <seaboyz@hotmail.com>
2022-08-22 10:32:46 -0400

    Inheritance joined table

--- src/main/java/com/webdev/dto/Vehicle.java
+++ src/main/java/com/webdev/dto/Vehicle.java
@@ -8,7 +8,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Vehicle {
 
     @Id
