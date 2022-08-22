af175ab57914103ba664cfa0c76fe8e1eef3cfec
seaboyz <seaboyz@hotmail.com>
2022-08-21 20:03:28 -0400

    Remove Address

--- src/main/java/com/webdev/App.java
+++ src/main/java/com/webdev/App.java
@@ -4,32 +4,14 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.webdev.dto.Address;
 import com.webdev.dto.UserDetails;
 
 public class App {
     public static void main(String[] args) {
         UserDetails user = new UserDetails();
 
-        Address address1 = new Address();
-        address1.setStreet("123 Main Street");
-        address1.setCity("Anytown");
-        address1.setState("CA");
-        address1.setZip("12345");
-
-        Address address2 = new Address();
-        address2.setStreet("456 Office Street");
-        address2.setCity("Anytown");
-        address2.setState("CA");
-        address2.setZip("12345");
-
-        user.getListOfAddresses().add(address1);
-        user.getListOfAddresses().add(address2);
-
         user.setUserId(1);
         user.setUserName("first user");
-        user.setJoinedDate(new java.sql.Date(new java.util.Date().getTime()));
-        user.setDescription("First User's description");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
@@ -37,12 +19,6 @@ public class App {
         session.save(user);
         session.getTransaction().commit();
         session.close();
-
-        user = null;
-        session = sessionFactory.openSession();
-        user = (UserDetails) session.get(UserDetails.class, 1);
-        session.close();
-        System.out.println(user.getListOfAddresses().size());
 
     }
 }
--- src/main/java/com/webdev/dto/UserDetails.java
+++ src/main/java/com/webdev/dto/UserDetails.java
@@ -1,18 +1,10 @@
 package com.webdev.dto;
 
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Collection;
-
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.Table;
 
 @Entity
@@ -26,39 +18,6 @@ public class UserDetails {
 
     @Column(name = "user_name")
     private String userName;
-
-    @Column(name = "joined_date")
-    private Date joinedDate;
-
-    private String description;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_address", joinColumns = @JoinColumn(name = "user_id"))
-    private Collection<Address> listOfAddresses = new ArrayList<Address>();
-
-    public Collection<Address> getListOfAddresses() {
-        return this.listOfAddresses;
-    }
-
-    public void setListOfAddresses(Collection<Address> listOfAddresses) {
-        this.listOfAddresses = listOfAddresses;
-    }
-
-    public Date getJoinedDate() {
-        return this.joinedDate;
-    }
-
-    public void setJoinedDate(Date joinedDate) {
-        this.joinedDate = joinedDate;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     public int getUserId() {
         return this.userId;
