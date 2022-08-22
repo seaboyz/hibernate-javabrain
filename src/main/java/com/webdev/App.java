1dfcbc3addfd3e14a006436b9c6a2f376a45a478
seaboyz <seaboyz@hotmail.com>
2022-08-22 14:24:19 -0400

    Change from detached to persistent

--- src/main/java/com/webdev/App.java
+++ src/main/java/com/webdev/App.java
@@ -9,20 +9,29 @@ import com.webdev.dto.UserDetails;
 public class App {
     public static void main(String[] args) {
 
-        UserDetails user = new UserDetails();
-        user.setUserName("Test User");
-
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        session.save(user);
-        user.setUserName("Updated User");
-        user.setUserName("Updated User Agian");
+        UserDetails user = (UserDetails) session.get(UserDetails.class, 1);
+        System.out.println(user.getUserName());
 
         session.getTransaction().commit();
         session.close();
 
-        user.setUserName("Updated User Again After Session Closed");
+        user.setUserName("Updated User Name After Session Close");
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.update(user);
+
+        user.setUserName("Updated User Name After Session Open");
+
+        session.getTransaction().commit();
+        session.close();
+
+        System.out.println(user.getUserName());
+
     }
 }
